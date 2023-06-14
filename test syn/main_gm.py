2a7877c772a026a05bfd7d58c4e82b556be01dbc
Peter Gultom <pgultom@mines.edu>
2023-06-13 20:19:23 -0600

    Update main_gm.py

--- test syn/main_gm.py
+++ test syn/main_gm.py
@@ -11,8 +11,8 @@ from utils import get_loops, get_dataset, get_network, get_eval_pool, evaluate_s
 def main():
 
     parser = argparse.ArgumentParser(description='Parameter Processing')
-    parser.add_argument('--dataset', type=str, default='CIFAR10', help='dataset')
-    parser.add_argument('--model', type=str, default='ConvNet', help='model')
+    parser.add_argument('--dataset', type=str, default='FashionMNIST', help='dataset')
+    parser.add_argument('--model', type=str, default='ResNet18', help='model')
     parser.add_argument('--ipc', type=int, default=1, help='image(s) per class')
     parser.add_argument('--eval_mode', type=str, default='S', help='eval_mode') # S: the same to training model, M: multi architectures,  W: net width, D: net depth, A: activation function, P: pooling layer, N: normalization layer,
     parser.add_argument('--num_exp', type=int, default=5, help='the number of experiments')
